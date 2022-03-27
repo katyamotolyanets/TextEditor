9e6218c113c468dea18b3a1d5ad0f8d42d5fe6e0
katya <katiamotolianec@gmail.com>
2022-03-27 15:25:52 +0300

    change url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const DEBUG = false
-let URL = "https://stark-brook-17339.herokuapp.com//uploads"
+let URL = "https://stark-brook-17339.herokuapp.com/uploads"
 
 if (DEBUG) {
     URL = "http://localhost:8000/uploads"
