9abe9cc706dc1b0a3c35524035ca0e40e0ea64bb
katya <katiamotolianec@gmail.com>
2022-03-27 12:46:33 +0300

    update config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-DEBUG = false
-URL = "https://gregarious-platypus-97d9bd.netlify.app/uploads"
+const DEBUG = false
+let URL = "https://gregarious-platypus-97d9bd.netlify.app/uploads"
 
 if (DEBUG) {
     URL = "http://localhost:8000/uploads"
